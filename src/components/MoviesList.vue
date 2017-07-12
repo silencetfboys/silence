10872e3f4b8e9255cc35b374df18b9b83740fa1c
silence <2457123264@qq.com>
2017-07-12 16:50:02 +0800

    silence

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="moviesList">
+    	<p>{{message.subject_collection.name}}</p>
+    	<div id="goodsLIst">
+    		<ul v-for="item01 in message.subject_collection_items">
+    			<li><img :src="item01.cover.url" alt=""></li>
+    		</ul>
+    	</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'moviesList',
+  data(){
+      return {
+          
+      }
+  },
+  props:{
+      message:{
+      	type:Array,
+      	default:function () {
+      		return [];
+      	}
+      }
+  },
+  mounted(){
+  }  
+}
+</script>
+
+<style>
+</style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="movies">
-    <p>movies</p>
+    <MoviesList :message="arr1"></MoviesList>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'movies'
+import MoviesList from '../components/MoviesList'
+	export default {
+  		name: 'movies',
+  	  	data(){
+      		return {
+ 						arr1:[],		
+      		}
+  		},
+    	components:{
+    		MoviesList
+  		},
+  		mounted(){
+	    	this.$http.get("//m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items?os=ios&callback=?").then(function(res){
+        		this.arr1=res.data;
+        		console.log(this.arr1)
+      			},function(){
+      		});
+  		}
 }
+
 </script>
 
 <style>
